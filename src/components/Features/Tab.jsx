eb17b455f15a126017bde9baee7e0eeb0f013575
Jay Wes <wesleyjay202@gmail.com>
2024-09-13 12:51:45 +0300

    Bug Fix

--- src/components/Features/Tab.jsx
+++ src/components/Features/Tab.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const Tab = ({ label, target, onClick, isActive }) => (
 	<div
-		className={`flex justify-center text-center cursor-pointer text-gray-600 border-b md:border-b-0 w-1/4 
-			${isActive ? 'text-earthYellow border-earthYellow' : 'hover:text-earthYellow'}`}
+		className={`flex justify-center items-center cursor-pointer text-gray-600 border-b md:border-b-0 w-full md:w-1/4 
+      ${isActive ? 'text-earthYellow border-earthYellow' : 'hover:text-earthYellow'}`}
 		onClick={() => onClick(target)}
 	>
 		<div
-			className={`py-5 border-b-4 ${
-				isActive ? 'border-earthYellow' : 'border-transparent'
-			}`}
+			className={`py-3 md:py-5 border-b-4 w-full text-center ${isActive ? 'border-earthYellow' : 'border-transparent'
+				}`}
 			data-target={target}
 		>
 			{label}
